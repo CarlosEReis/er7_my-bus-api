d6ffba176e2e582467b8b46c7b75b9ac4a496f9d
CarlosEReis <carlos.er7@gmail.com>
2023-12-22 22:05:30 -0300

    build: criacao do projeto

---
+++ src/main/java/com/carloser7/mybus/MyBusApplication.java
@@ -0,0 +1,13 @@
+package com.carloser7.mybus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloser7/mybus/MyBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloser7.mybus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
