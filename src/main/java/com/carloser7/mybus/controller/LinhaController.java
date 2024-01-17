d006ce75ff9d092f991da0ab5deb4f6dd1aadff6
CarlosEReis <carlos.er7@gmail.com>
2024-01-17 14:00:36 -0300

    feat: disable CORS temporarily

--- src/main/java/com/carloser7/mybus/controller/LinhaController.java
+++ src/main/java/com/carloser7/mybus/controller/LinhaController.java
@@ -5,13 +5,11 @@ import com.carloser7.mybus.entity.Route;
 import com.carloser7.mybus.model.Linha;
 import com.carloser7.mybus.repository.LinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(originPatterns = "*")
 @RestController
 @RequestMapping("/linhas")
 public class LinhaController {
