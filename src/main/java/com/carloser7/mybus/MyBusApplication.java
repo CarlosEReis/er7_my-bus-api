278c36a965657e196178bdabc3d306fe868aa4f7
CarlosEReis <carlos.er7@gmail.com>
2024-01-16 22:16:45 -0300

    build: add bean RestTemplate

--- src/main/java/com/carloser7/mybus/MyBusApplication.java
+++ src/main/java/com/carloser7/mybus/MyBusApplication.java
@@ -2,6 +2,9 @@ package com.carloser7.mybus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MyBusApplication {
@@ -10,4 +13,8 @@ public class MyBusApplication {
 		SpringApplication.run(MyBusApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
