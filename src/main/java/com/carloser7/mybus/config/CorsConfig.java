7435373fa3261b9b854b405f71c72af142f92225
CarlosEReis <carlos.er7@gmail.com>
2025-06-12 11:53:24 -0300

    refactor: atualiza confg cors
    - Remove config do controller e add na classe

---
+++ src/main/java/com/carloser7/mybus/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.carloser7.mybus.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${com.er7.mybus.api.cors.allowedOrigins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
