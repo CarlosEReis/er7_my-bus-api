f278b97c980c8dec18f6218ffab3d5d80f091133
CarlosEReis <carlos.er7@gmail.com>
2025-07-09 21:34:03 -0300

    feat: add log dos cors permitidos

--- src/main/java/com/carloser7/mybus/config/CorsConfig.java
+++ src/main/java/com/carloser7/mybus/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.carloser7.mybus.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    private static final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
+
     @Value("${com.er7.mybus.api.cors.allowedOrigins}")
     private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        logger.info("CORS allowed origins configuration: {}", allowedOrigins);
         registry.addMapping("/**")
-            .allowedOrigins(allowedOrigins)
+            .allowedOrigins(allowedOrigins.split(","))
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
